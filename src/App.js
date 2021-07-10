657ba32d8afe917ba016269f7973a9864fdc5817
criaturaimaginaria <80127608+criaturaimaginaria@users.noreply.github.com>
2021-07-10 16:11:08 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import FetchBtc from './components/FetchBtc';
+
+import FetchBtc3 from './components/fetchBtc3';
+
+
+function App() {
+  return (
+    <div className="App">
+       <FetchBtc3 />
+       <FetchBtc />
+    
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/fetchBtc3.jsx
@@ -0,0 +1,34 @@
+import React,{useState, useEffect} from 'react'
+
+
+const FetchBtc3 = () => {
+    const BtcUrl  = 'https://api.coindesk.com/v1/bpi/currentprice.json' 
+    const gif = 'https://media.tenor.com/images/a742721ea2075bc3956a2ff62c9bfeef/tenor.gif'
+    const [api, setApi] = useState([])
+
+
+    const fetchBtcApi = async () =>{
+        const response = await fetch(BtcUrl)
+        const data = await response.json()
+        const number = (JSON.parse(data.bpi.USD.rate_float))    
+        setApi(number)
+    }
+
+   
+  useEffect(()=>{
+        setInterval(fetchBtcApi, 1000 )   
+
+    }, )
+    
+        return (
+            <div >
+                    <h1> { api == 0  ?  <img src={gif} className="gif"/> : Math.trunc(api)  }  </h1>       
+                   
+            </div>
+        )
+    }
+    export default FetchBtc3
+
+    
+    // video tutorial call api
+    // https://www.youtube.com/watch?v=kZ4YaoCYais&ab_channel=PabloLizardo
