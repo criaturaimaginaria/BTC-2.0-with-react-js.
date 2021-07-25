55ba446e5d03a587bac962ee45f72193def99243
criaturaimaginaria <80127608+criaturaimaginaria@users.noreply.github.com>
2021-07-25 16:16:06 -0300

    .

--- src/components/OtherCurrency.jsx
+++ src/components/OtherCurrency.jsx
@@ -6,7 +6,6 @@ import './OtherCurrency.css'
 const OtherCurrency = () => {
     const cryptoUrl  = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
 
-//   -------------------------LIST------------------------------  
     const [api, setApi] = useState([])
 
     const marketapi = async () =>{
@@ -14,7 +13,7 @@ const OtherCurrency = () => {
         const data = await response.json() 
 
         const number = (data) 
-        console.log(number)    
+        // console.log(number)    
         setApi(number)   
     }
 
@@ -22,19 +21,40 @@ const OtherCurrency = () => {
         marketapi()
     },[] )
 
-    const hello = "gello"
+// stack overflow algorithm ---------------------------------
+    // function abbreviateNumber(value) {
+    //     var newValue = value;
+    //     if (value >= 1000) {
+    //         let suffixes = ["", "k", "m", "b","t"];
+    //         let suffixNum = Math.floor( (""+value).length/3 );
+    //         let shortValue = '';
+    //         for (let precision = 2; precision >= 1; precision--) {
+    //             shortValue = parseFloat( (suffixNum != 0 ? (value / Math.pow(1000,suffixNum) ) : value).toPrecision(precision));
+    //             let dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g,'');
+    //             if (dotLessShortValue.length <= 3) { break; }
+    //         }
+    //         if (shortValue % 1 != 0)  shortValue = shortValue.toFixed(1);
+    //         newValue = shortValue+suffixes[suffixNum];
+    //     }
+    //     return newValue;
+    // }
+//  ----------------------------------------------------------   
+
+function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
+  }
 
     const tableData = (api) =>{
         return(
             <tr key={api.id} >
                 <td>{<img src={api?.image }  alt="logos" className="logos"/>   }</td>  
                 <td>  { <div className="symbol"> {api.symbol} </div>}   { <div className="name"> {api.name } </div> }   </td>
-                <td></td> 
+                <td>{ formatNumber(api.market_cap)  }</td>
                 <td>{api.current_price}</td>
                 <td>{api.ath}</td>
                 <td>  { api.price_change_percentage_24h > 0 ?   
-                <p style={{color:"green"}} >  {api.price_change_percentage_24h } </p>
-                : <p style={{color:"red"}} > {api.price_change_percentage_24h } </p>  }  </td>
+                <p style={{color:"green"}} >  {api.price_change_percentage_24h.toFixed(2) } </p>
+                : <p style={{color:"red"}} > {api.price_change_percentage_24h.toFixed(2) } </p>  }  </td>
             </tr>
         )
     }
@@ -43,7 +63,8 @@ const OtherCurrency = () => {
 
    
         <div className="currencies">
-            <h1>other currencies</h1>
+           
+
             
 
                 <div className="div-container">
@@ -52,9 +73,9 @@ const OtherCurrency = () => {
                   <table>
                       <thead>
                           <tr>
-                            <th>NAME</th> 
-                            <th></th>
-                            <th></th>
+                            <th> O</th> 
+                            <th>NAME</th>
+                            <th>MARKET CAP</th>
                             <th>PRICE</th>
                             <th>ATH</th>
                             <th>24H CHANGE</th>
@@ -71,13 +92,9 @@ const OtherCurrency = () => {
 
                 </div>
           
-                   
-               
-   
 
             </div>
-
-           
+        
               
     )
 }
