31534f0164c912f349cb4b1dc96e44b67c7b13ec
criaturaimaginaria <80127608+criaturaimaginaria@users.noreply.github.com>
2021-08-01 17:35:25 -0300

    main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Main.css'
 
 const Main = () => {
+        const  [title, setTitle] = useState()
+
+    useEffect(() => {
+
+        setTimeout(()=>{setTitle(true)}, 750);
+     
+        // setTitle(true)
+    }, [] )
 
     
+
     return (
         <div className="main">
-            <h1>Main</h1>
+            <h1 className={title ? "title animated" : "title"}>Main</h1>
         
         </div>
     )
