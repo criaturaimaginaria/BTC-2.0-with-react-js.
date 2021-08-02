ee1bfd36465eef58487269428a9b6577fee4f309
criaturaimaginaria <80127608+criaturaimaginaria@users.noreply.github.com>
2021-08-02 20:49:26 -0300

    .

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,34 +5,29 @@ import './Main.css'
 const Main = () => {
         const  [title, setTitle] = useState()
         const  [window, setWindow] = useState()
-        // const  [motionBack, setMotionBack] = useState()
+        const  [motionBack, setMotionBack] = useState()
         
 
     useEffect(() => {
 
         setTimeout(()=>{setTitle(true)}, 750);
-        setTimeout(()=>{setWindow(true)}, 900);
-        // setTimeout(()=>{setMotionBack(true)}, 9000);
+        setTimeout(()=>{setWindow(true)}, 1400);
+        setTimeout(()=>{setMotionBack(true)}, 600);
     }, [] )
 
     
 
     return (
         <div className="main">
-            <h1 className={title ? "animated-title" : "title"}>Decentralized money</h1>
+            <h1 className={title ? "animated-title" : "title"}>Decentralized money Aa</h1>
 
-        <div className={window ? "back_background-animated" : "back_background"}>
-              <div className="presentation">
-                 
-                  <div className={window ? "animated-window-one" : "window-one"}></div>
-                  <div className={window ? "animated-window-two" : "window-two"}> </div>
-
-                
-                
+            <div className={window ? "back_background-animated" : "back_background"}>
+                <div className="presentation">
+                    <div className={motionBack ? "animated-window-one" : "window-one"}></div>
+                    <div className={motionBack ? "animated-window-two" : "window-two"}> </div>
+                </div>
             </div>
-        </div>
-            
-        
+  
         </div>
     )
 }
